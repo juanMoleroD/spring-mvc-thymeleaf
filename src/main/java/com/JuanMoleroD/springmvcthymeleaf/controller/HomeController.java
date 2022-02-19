388527384fab8197414beae1176e37a4efa8f06e
juanMoleroD <juanldimeco@gmail.com>
2022-02-19 09:09:49 +0000

    Initial commit

---
+++ src/main/java/com/JuanMoleroD/springmvcthymeleaf/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.JuanMoleroD.springmvcthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(@RequestParam(required = false) String login, Model model){
+        System.out.println("Login is: " + login);
+        model.addAttribute("login", login);
+
+        return "home_page";
+    }
+}
---
+++ src/test/java/com/JuanMoleroD/springmvcthymeleaf/SpringMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JuanMoleroD.springmvcthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
